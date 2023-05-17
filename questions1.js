6d58e849e5359871866fe458b9eb729059ed27ce
Ntando Elton <77546309+Eltonntando@users.noreply.github.com>
2023-05-17 11:34:56 +0200

    Update questions1.js

--- questions1.js
+++ questions1.js
@@ -2,28 +2,6 @@
 let questions = [
     {
     numb: 1,
-    question: "What is the name of the story book you were reading?",
-    answer: "Little In A Big World",
-    options: [
-      "Big In A Little World",
-      "Small In A Big World",
-      "Little In A Big World",
-      "The Little Ones In A Big World"
-    ]
-  },
-    {
-    numb: 2,
-    question: "How many stories does the story book have?",
-    answer: "7 Stories",
-    options: [
-      "6 Stories",
-      "10 Stories",
-      "5 Stories",
-      "7 Stories"
-    ]
-  },
-    {
-    numb: 3,
     question: "What is the name of the boy who was a soccer star?",
     answer: "Simo",
     options: [
@@ -33,19 +11,74 @@ let questions = [
       "Messi"
     ]
   },
+     {
+    numb: 2,
+    question: "What is the nickname that Simo's father gave Simo?",
+    answer: "Messi",
+    options: [
+      "Ronaldo",
+      "Ronaldihno",
+      "Messi",
+      "Massy"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Lionel Messi is a soccer star from wich Country?",
+    answer: "Argentina",
+    options: [
+      "Argentina",
+      "Portugal",
+      "South Africa",
+      "England"
+    ]
+  },
     {
     numb: 4,
-    question: "With howmany goals did Simo's team win the game by?",
-    answer: "4-0",
+    question: "Where did the coach first saw Simo Playing soccer?",
+    answer: "At his home backyard",
     options: [
-      "4-2",
-      "5-1",
-      "6-0",
-      "4-0"
+      "At his school sports ground",
+      "Near the road",
+      "At the sports field",
+      "At his home backyard"
     ]
   },
     {
     numb: 5,
+    question: "Where was the coach coming from when he saw Simo playing?",
+    answer: "Coming from practice at the sports ground",
+    options: [
+      "Coming from practice at the sports ground",
+      "From his home",
+      "From the shops",
+      "Coming from the local school"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What did the coach do after seing Simo play outside?",
+    answer: "He invited Simo to come join his team",
+    options: [
+      "He continued on his way home",
+      "He invited Simo to come join his team",
+      "He went back to the sports ground",
+      "He shouted 'Go boy!'"
+    ]
+  },
+    {
+    numb: 7,
+    question: "On what day was the match betwwen the two teams?",
+    answer: "Saturday",
+    options: [
+      "On a Sunday",
+      "On a Monday",
+      "On a Tuesday",
+      "On a Saturday"
+    ]
+  },
+    {
+    numb: 8,
     question: "What is the colour of the Jersey Simo's team was wearing?",
     answer: "Blue",
     options: [
@@ -55,6 +88,84 @@ let questions = [
       "Red"
     ]
   },
+  {
+    numb: 9,
+    question: "What is the colour of the soccer boots Simo's father bought him?",
+    answer: "Black",
+    options: [
+      "Black",
+      "Blue",
+      "Orange",
+      "Red"
+    ]
+  },
+    {
+    numb: 10,
+    question: "How was Simo feeling at the beginning of the game?",
+    answer: "Scared",
+    options: [
+      "Scared",
+      "Happy",
+      "Sad",
+      "Angry"
+    ]
+  },
+    {
+    numb: 11,
+    question: "How did Simo play at the beginning of the game?",
+    answer: "He was too bad at controlling the ball",
+    options: [
+      "He was very good at controlling the ball",
+      "He was too bad at controlling the ball",
+      "He tried his best",
+      "He was very impressive at controlling the ball"
+    ]
+  },
+    {
+    numb: 12,
+    question: "What did Simo's father tell Simo after seing he was afraid?",
+    answer: "Do not be afraid, do your best son!",
+    options: [
+      "You disapoint me son!",
+      "Let's go home since you are embarasing yourself!",
+      "Do not be afraid, do your best son!",
+      "Do not to your best, just quite!"
+    ]
+  },
+    {
+    numb: 13,
+    question: "How did Simo feel after his father encouraged him?",
+    answer: "He felt good and wanted to be better",
+    options: [
+      "He did not listed and continued playing",
+      "He felt sad and wanted to go home",
+      "He felt bad and wanted to cry",
+      "He felt good and wanted to be better"
+    ]
+  },
+    {
+    numb: 14,
+    question: "With howmany goals did Simo's team win the game by?",
+    answer: "4-2",
+    options: [
+      "4-2",
+      "5-1",
+      "6-0",
+      "4-0"
+    ]
+  },
+    {
+    numb: 15,
+    question: "What lesson did Simo learn",
+    answer: "He learned that to win you must not be scared, but calm down and be your best!",
+    options: [
+      "He learned that to win you must not be scared, but calm down and be your best!",
+      "He learned that you can be scared, and continue with being bad at what you do",
+      "He learned that you must run home when you are feeling scared",
+      "He learned that to win you must be scared, not calm down and not be your best!"
+    ]
+  },
+
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
   //   {
