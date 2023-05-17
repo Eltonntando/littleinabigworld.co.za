8e3d6f007cf94d42e92c61e35be8328f4d7932d1
Ntando Elton <77546309+Eltonntando@users.noreply.github.com>
2023-05-17 11:36:31 +0200

    Update questions3.js

--- questions3.js
+++ questions3.js
@@ -2,57 +2,112 @@
 let questions = [
     {
     numb: 1,
-    question: "What is the name of the story book you were reading?",
-    answer: "Little In A Big World",
+    question: "What is the name of the young boy in this story?",
+    answer: "Wandile",
     options: [
-      "Big In A Little World",
-      "Small In A Big World",
-      "Little In A Big World",
-      "The Little Ones In A Big World"
+      "Khulani",
+      "Troy",
+      "Wandile",
+      "Simo"
     ]
   },
     {
     numb: 2,
-    question: "How many stories does the story book have?",
-    answer: "7 Stories",
+    question: "What did Wandile enjoy looking at during the night?",
+    answer: "The stars and moon in the night sky",
     options: [
-      "6 Stories",
-      "10 Stories",
-      "5 Stories",
-      "7 Stories"
+      "The sun in the night sky",
+      "The stars and moon in the night sky",
+      "The sun in the day sky",
+      "The stars and moon in the day sky"
     ]
   },
     {
     numb: 3,
-    question: "What is the name of the boy who was a soccer star?",
-    answer: "Simo",
+    question: "Did Wandile's mother know about his son's love for exploring the sky?",
+    answer: "Yes she did",
     options: [
-      "Simo",
-      "Vuyo",
-      "Thabo",
-      "Messi"
+      "Yes she did",
+      "No she did not know",
+      "She did not care about it",
+      "She ignored that"
     ]
   },
     {
     numb: 4,
-    question: "With howmany goals did Simo's team win the game by?",
-    answer: "4-0",
+    question: "What did Wandile's mother by him as a suprise?",
+    answer: "A Telescope",
     options: [
-      "4-2",
-      "5-1",
-      "6-0",
-      "4-0"
+      "A digital camera",
+      "A car toy",
+      "A cellphone",
+      "A Telescope"
     ]
   },
     {
     numb: 5,
-    question: "What is the colour of the Jersey Simo's team was wearing?",
-    answer: "Blue",
+    question: "A Telescope is a tool for?",
+    answer: "For seing faraway objects",
     options: [
-      "Yellow",
-      "Blue",
-      "Orange",
-      "Red"
+      "For seing nearby objects",
+      "For seing faraway objects",
+      "For seing new objects",
+      "For seing invisible objects"
+    ]
+  },
+    {
+    numb: 6,
+    question: "How many planets does our solar system has?",
+    answer: "9 planets",
+    options: [
+      "12 planets",
+      "10 planets",
+      "6 planets",
+      "9 planets"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Planet Earth has how may moons?",
+    answer: "1 moon",
+    options: [
+      "2 moons",
+      "1 moon",
+      "3 moons",
+      "9 moons"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Which order of planets is correct for our solar system?",
+    answer: "1.Mercury 2.Venus 3.Earth 4.Mars 5.Jupiter 6.Saturn 7.Uranus 8.Neptune 9.Pluto",
+    options: [
+      "1.Mercury 2.Venus 3.Earth 4.Mars 5.Jupiter 6.Saturn 7.Uranus 8.Neptune 9.Pluto",
+      "1.Earth 2.Mars 3.Jupiter 4.Saturn 5.Uranus 6.Neptune 7.Mercury 8.Venus 9.Pluto",
+      "1.Neptune 2.Pluto 3.Mercury 4.Venus 5.Earth 6.Mars 7.Jupiter 8.Saturn 9.Uranus",
+      "1.Jupiter 2.Saturn 3.Uranus 4.Neptune 5.Pluto 6.Mercury 7.Venus 8.Earth 9.Mars"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Planet Earth our home it the...?",
+    answer: "Is the 3rd plannet on the solar system orbit",
+    options: [
+      "Is the 5th plannet on the solar system orbit",
+      "Is the 4th plannet on the solar system orbit",
+      "Is the 2nd plannet on the solar system orbit",
+      "Is the 3rd plannet on the solar system orbit"
+    ]
+  },
+    {
+    numb: 10,
+    question: "How did Wandile feel about his new Telescope?",
+    answer: "He was very happy and wanted to use it right away",
+    options: [
+      "He was very sad and wanted to break right away",
+      "He was very happy and wanted to use it right away",
+      "He was very angry because it is not what he wanted",
+      "He was mad at his mother and he cried"
     ]
   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
